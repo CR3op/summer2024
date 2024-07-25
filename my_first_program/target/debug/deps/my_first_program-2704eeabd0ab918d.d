be1aa69e87659514421c8e50f5812cf38b5d00fd
Carlos Roldan <120054943+CR3op@users.noreply.github.com>
2024-07-25 21:48:23 +0000

    finished the linux command terminal

---
+++ my_first_program/target/debug/deps/my_first_program-2704eeabd0ab918d.d
@@ -0,0 +1,5 @@
+/workspaces/summer2024/my_first_program/target/debug/deps/my_first_program-2704eeabd0ab918d: src/main.rs
+
+/workspaces/summer2024/my_first_program/target/debug/deps/my_first_program-2704eeabd0ab918d.d: src/main.rs
+
+src/main.rs:
